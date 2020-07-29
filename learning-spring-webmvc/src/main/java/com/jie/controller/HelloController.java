d0cd93de72a857d86e428b45df55043ae416f348
liujiee <lj_soft@126.com>
2020-07-29 14:44:28 +0800

    update spring mvc project

--- learning-spring-webmvc/src/main/java/com/jie/controller/HelloController.java
+++ learning-spring-webmvc/src/main/java/com/jie/controller/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/hello")
 public class HelloController {
 
-	@GetMapping("/hello")
+	@GetMapping
 	public String hello() {
 		return "Hello world";
 	}
